c1f5f640f3153125ae9bfd2c49f477c6da795f32
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-11-04 22:24:05 -0500

    Connect the app to a database and handle unhandled rejections promise

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 
+const connectDB = require('./config/db');
 const bootcampsRoute = require('./routes/bootcamps');
 
 // Load env variables
 dotenv.config();
 
 const app = express();
+
+// Connect to the database
+connectDB();
 
 // Middleware
 if (process.env.NODE_ENV === 'development') {
@@ -19,6 +23,13 @@ app.use('/api/v1/bootcamps', bootcampsRoute);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App is running in ${process.env.NODE_ENV} on port: ${PORT}`);
+});
+
+// Handle  unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close server and exit process
+  server.close(() => process.exit(1));
 });

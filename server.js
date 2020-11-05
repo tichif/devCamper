61106adbad2614475dc05876663f60f187a330a8
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-11-04 22:35:18 -0500

    Install colors for the console

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 
 const connectDB = require('./config/db');
 const bootcampsRoute = require('./routes/bootcamps');
@@ -24,12 +25,14 @@ app.use('/api/v1/bootcamps', bootcampsRoute);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-  console.log(`App is running in ${process.env.NODE_ENV} on port: ${PORT}`);
+  console.log(
+    `App is running in ${process.env.NODE_ENV} on port: ${PORT}`.yellow.bold
+  );
 });
 
 // Handle  unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // close server and exit process
   server.close(() => process.exit(1));
 });

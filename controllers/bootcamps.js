93c05ea363c19444f1c0de05e60ef8ac1cf27b92
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-11-05 10:55:48 -0500

    Fetch bootcamps or single bootcamp by ID

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,29 @@ const Bootcamp = require('../models/Bootcamp');
 // @route   GET /api/v1/bootcamps
 // @desc    Get all bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Get all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @route   GET /api/v1/bootcamps/:id
 // @desc    Get a single bootcamp
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(404).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @route   POST /api/v1/bootcamps

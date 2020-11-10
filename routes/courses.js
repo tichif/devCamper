fd05abf1449f2eae57510c4cd85d6ebdc6a1c566
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-11-10 18:00:19 -0500

    Create an role protection middleware

--- routes/courses.js
+++ routes/courses.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const Course = require('../models/Course');
 const advancedResults = require('../middleware/advancedResults');
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 const {
   getCourses,
@@ -23,12 +23,12 @@ router
     }),
     getCourses
   )
-  .post(protect, addCourse);
+  .post(protect, authorize('publisher', 'admin'), addCourse);
 
 router
   .route('/:id')
   .get(getCourse)
-  .put(protect, updateCourse)
-  .delete(protect, deleteCourse);
+  .put(protect, authorize('publisher', 'admin'), updateCourse)
+  .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;

a9ab2a2e16788a1fc857b3c4dd37467a2b33ca04
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-11-10 23:08:05 -0500

    Send email with url to reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const ErrorResponse = require('../util/errorResponse');
+const sendEmail = require('../util/sendEmail');
 const asyncHandler = require('../middleware/async');
 
 // @route   POST /api/v1/auth/register
@@ -74,10 +75,33 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
   await user.save({ validateBeforeSave: false });
 
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
+  // Create reset URL
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has requested the reset of the password. Please make a put request to: \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      message,
+      subject: 'Password Reset Token',
+    });
+
+    res.status(200).json({
+      success: true,
+      data: 'Email sent',
+    });
+  } catch (err) {
+    console.log(err);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse('Email could not be sent', 500));
+  }
 });
 
 // Get token from model and create cookie and send response
---
+++ util/sendEmail.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USERNAME,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  const message = {
+    from: `${process.env.FROM_EMAIL} <${process.env.FROM_NAME}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  const info = await transporter.sendMail(message);
+
+  console.log('Message sent: %s', info.messageId);
+};
+
+module.exports = sendEmail;
